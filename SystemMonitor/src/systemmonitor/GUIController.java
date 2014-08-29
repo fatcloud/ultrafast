eda3e5771ec3e99d728b6194b31af25914483293
fatcloud <victor19871226@hotmail.com>
2014-08-30 07:33:40 +0800

    knowing how to add items to choiceboxes and use event.getSource to know which button was clicked now

--- SystemMonitor/src/systemmonitor/GUIController.java
+++ SystemMonitor/src/systemmonitor/GUIController.java
@@ -8,9 +8,12 @@ package systemmonitor;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 
 /**
@@ -19,21 +22,35 @@ import javafx.scene.control.Label;
  */
 public class GUIController implements Initializable {
     HelloThread t1;
+    @FXML private ChoiceBox MotorIDChoice;
+    
     
     @FXML
     private Label label;
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
-        t1.terminate();
+        Button btn = (Button) event.getSource();
+        String btnText = btn.getText();
+        if( btnText.equals("+10") )
+            t1.terminate();
+        else if( btnText.equals("+1") ) {
+            t1 = new HelloThread();
+            t1.setName("Oh hahahaha I am a thread");
+            t1.start();
+        } else if( btnText.equals("-1") )
+            MotorIDChoice.setItems(FXCollections.observableArrayList("Energy Tuner","autocorelator"));
+            
+        
+        
     }
     
     @FXML
     private void handleButtonAction2(ActionEvent event) {
-        t1 = new HelloThread();
-        t1.setName("Oh hahahaha I am a thread");
-        t1.start();
+        System.out.println("maomao");
+        
     }
+    
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
