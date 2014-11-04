08da588ef26e28e5c7432061d1f6cb8484c706c1
Admin <Admin@223Public>
2014-11-04 19:20:33 +0800

    Serial communication has been added.

--- SystemMonitor/src/systemmonitor/GUIController.java
+++ SystemMonitor/src/systemmonitor/GUIController.java
@@ -15,6 +15,9 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import Serialcommunicator.Serialcommunicator;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -26,15 +29,28 @@ public class GUIController implements Initializable {
     @FXML private ChoiceBox MotorIDChoice;
     @FXML private ChoiceBox MotorFeedbackCameraChoice;
     
+    Serialcommunicator myserial = new Serialcommunicator();
+    
+    
+    
     @FXML private void handleButtonAction(ActionEvent event) {
         Button btn = (Button) event.getSource();
         String btnText = btn.getText();
         
+        //HelloThread t1;
+        //t1 = new HelloThread();
+        
         byte stepToGo = 0;
-        if( btnText.equals("+10") ) {
-            stepToGo = 10;
+        if( btnText.equals("+10") ) {            
+            //stepToGo = 10;
+            //t1.start();
+            
+            myserial.writeData('a'); // Why can it not know myserial?
+            
+            
         } else if( btnText.equals("+1") ) {
             stepToGo = 1;
+            //t1.terminate();
         } else if( btnText.equals("-1") ){
             stepToGo = -1;
         } else if( btnText.equals("-10") ){
@@ -57,5 +73,21 @@ public class GUIController implements Initializable {
         System.out.println("haha");
         MotorIDChoice.setItems(FXCollections.observableArrayList(0,1,2,3));
         MotorIDChoice.setValue( MotorIDChoice.getItems().get(0) );
+        
+        HelloThread t1;
+        t1 = new HelloThread();
+        
+        
+        
+        myserial.initialize();
+        System.out.println("Started");
+
+        try {        
+            Thread.sleep(10000); System.out.println("main thread"); //to be improved.
+        } catch (InterruptedException ex) {
+            Logger.getLogger(GUIController.class.getName()).log(Level.SEVERE, null, ex);
+        }        
+            
+        
     }    
 }
